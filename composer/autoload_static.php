b58bafa8cb14294aa6943ee43b970f086f2e4074
tailieuweb <tailieuweb.com@gmail.com>
2021-06-30 16:24:44 +0700

    test

--- composer/autoload_static.php
+++ composer/autoload_static.php
@@ -508,6 +508,7 @@ class ComposerStaticInit7997ba8f77937a83d1f609e4e7f1bcbb
         'App\\Http\\Middleware\\TrustHosts' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustHosts.php',
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
+        'App\\Jobs\\SendingEmail' => __DIR__ . '/../..' . '/app/Jobs/SendingEmail.php',
         'App\\Models\\User' => __DIR__ . '/../..' . '/app/Models/User.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
