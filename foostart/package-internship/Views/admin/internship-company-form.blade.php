433792fb0c0e493d1ba990ae1af1cc01b6523be4
tailieuweb <tailieuweb.com@gmail.com>
2024-04-10 17:01:50 +0700

    test

--- foostart/package-internship/Views/admin/internship-company-form.blade.php
+++ foostart/package-internship/Views/admin/internship-company-form.blade.php
@@ -176,7 +176,9 @@
                             <span class="input-group-addon" id="basic-addon1">
                                 Tỉnh/Thành phố
                             </span>
-                            {!! Form::select('location_province', [], null, ['class' => 'form-control',  'placeholder' => 'Chọn tỉnh/thành phố']) !!}
+                            {!! Form::select('location_province', [], null, ['class' => 'form-control',
+                                                                            'id' => 'city',
+                                                                            'placeholder' => 'Chọn tỉnh/thành phố']) !!}
                         </div>
                     </div>
                     <div class="col-md-4">
@@ -184,7 +186,9 @@
                             <span class="input-group-addon" id="basic-addon1">
                                 Quận/Huyện
                             </span>
-                            {!! Form::select('location_district', [], null, ['class' => 'form-control',  'placeholder' => 'Chọn quận/huyện']) !!}
+                            {!! Form::select('location_district', [], null, ['class' => 'form-control',
+                                                                             'id' => 'district',
+                                                                             'placeholder' => 'Chọn quận/huyện']) !!}
                         </div>
                     </div>
                     <div class="col-md-4">
@@ -192,7 +196,9 @@
                             <span class="input-group-addon" id="basic-addon1">
                                 Xã/Phường
                             </span>
-                            {!! Form::select('location_ward', [], null, ['class' => 'form-control',  'placeholder' => 'Chọn xã/phường']) !!}
+                            {!! Form::select('location_ward', [], null, ['class' => 'form-control',
+                                                                             'id' => 'ward',
+                                                                             'placeholder' => 'Chọn xã/phường']) !!}
                         </div>
                     </div>
                 </div>
@@ -263,3 +269,47 @@
 <!------------------------------------------------------------------------------
 | End list of elements in company form
 |------------------------------------------------------------------------------>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"></script>
+<script>
+    var citis = document.getElementById("city");
+    var districts = document.getElementById("district");
+    var wards = document.getElementById("ward");
+    var Parameter = {
+        url: "https://raw.githubusercontent.com/kenzouno1/DiaGioiHanhChinhVN/master/data.json",
+        method: "GET",
+        responseType: "application/json",
+    };
+    var promise = axios(Parameter);
+    promise.then(function (result) {
+        renderCity(result.data);
+    });
+
+    function renderCity(data) {
+        for (const x of data) {
+            citis.options[citis.options.length] = new Option(x.Name, x.Id);
+        }
+        citis.onchange = function () {
+            district.length = 1;
+            ward.length = 1;
+            if(this.value != ""){
+                const result = data.filter(n => n.Id === this.value);
+
+                for (const k of result[0].Districts) {
+                    district.options[district.options.length] = new Option(k.Name, k.Id);
+                }
+            }
+        };
+        district.onchange = function () {
+            ward.length = 1;
+            const dataCity = data.filter((n) => n.Id === citis.value);
+            if (this.value != "") {
+                const dataWards = dataCity[0].Districts.filter(n => n.Id === this.value)[0].Wards;
+
+                for (const w of dataWards) {
+                    wards.options[wards.options.length] = new Option(w.Name, w.Id);
+                }
+            }
+        };
+    }
+</script>
